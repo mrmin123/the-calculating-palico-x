ac4547be716e8cdf0990d9354809b06e5f41e7ad
mrmin123 <minyoung.choi@gmail.com>
2016-09-01 22:50:07 -0700

    begin adding new css-only tooltips

--- calculator.js
+++ calculator.js
@@ -674,9 +674,9 @@ class CalculatingPalicoXInterface extends React.Component {
                                                                     return (
                                                                         <div className="row" key={k}>
                                                                             <div className="checkbox">
-                                                                                <label className={"col-xs-12 col-sm-12" + (currModifierGroup.indexOf(modifier) > -1 ? " modifiers-group" : "")}
+                                                                                <label className={"col-xs-12 col-sm-12 tooltip2" + (currModifierGroup.indexOf(modifier) > -1 ? " modifiers-group" : "")}
                                                                                     onMouseOver={() => this.modifierMouseOver(modifier)} onMouseLeave={this.modifierMouseLeave}
-                                                                                    data-toggle="tooltip" data-placement="bottom" title={modifiers[modifier].desc}>
+                                                                                    data-toggle="tooltip" data-placement="bottom" data-tooltip={modifiers[modifier].desc}>
                                                                                     <div className="col-xs-8 col-sm-8 modifiers-label text-right">{modifiers[modifier].name}</div>
                                                                                     <div className="col-xs-2 col-sm-2 modifiers-check">
                                                                                         <input type="checkbox" checked={selectedModifiers.indexOf(modifier) > -1 ? true : false} onChange={() => this.modifierSelection(modifier)} />
